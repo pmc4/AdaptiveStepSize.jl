11e73f84a0232cad915b3c135ce30ce6c0291192
Documenter.jl <documenter@juliadocs.github.io>
2025-05-05 09:26:47 +0000

    build based on 9a9a3b9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#AdaptiveStepSize","page":"Home","title":"AdaptiveStepSize","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AdaptiveStepSize.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
