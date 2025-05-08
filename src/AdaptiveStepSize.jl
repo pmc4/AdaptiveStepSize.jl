ad1927745594e578dabeed67183eae87e440e176
pmc4 <117096890+pmc4@users.noreply.github.com>
2025-05-08 15:27:34 +0200

    Improve docstrings in points_linear_singular
    Because
    * The explanation was not very precise

--- src/AdaptiveStepSize.jl
+++ src/AdaptiveStepSize.jl
@@ -135,15 +135,17 @@ See [`points_linear`](@ref) for an in depth description of the arguments. This f
 the additional argument `singularities`. This must be a `Vector{T}` where `T<:Real` that
 contains each singular point.
 
-A singular point is such that the function is not defined, like `log(x)` at x = 0. We can also
-use it for points where the second derivative is not continuous. For example `abs(x)` at x = 0.
+A singularity in this case is a point at which the function is not well-behaved, like `abs(x)` at x = 0, where the function is continuous but not differentiable.
 
-If the function is an piecewise function, we can write in the `singularities` vector those
-discontinuous points. The function `points_linear` will be applied at each subinterval. And
+If the function has several singularities, we can write those in the `singularities` vector. The function `points_linear` will be applied at each subinterval. In addition,
 the second derivative will never be computed at those endpoints, i.e., at the `singularities`
 and the `domain` points. Note that `f` **will be** evaluated at the endpoints and it should
-handle those discontinuities properly. It is the user responsability to do so, otherwise,
-call it with a value slightly larger or smaller.
+handle those discontinuities properly. It is the user responsability to do so.
+
+## Notes
+If you have a piecewise function, it might be convenient to apply [`points_linear`](@ref) at each interval of the function, instead of passing the `singularities` vector here. The reason is that the result contains just one big pair `xs` and `ys` vectors and it is not aware of the points where piecewise function is not continuous, hence producing undesiderable results in the interpolation.
+
+The safest bet here is linearly interpolate each region of the piecewise function separately.
 """
 function points_linear_singular(f, domain, singularities::Vector{T}, tol::T; scan_step = (domain[end] - domain[begin]) / 100) where T<:Real
     # Add the domain to the singularities. These are the new bounds for each step
